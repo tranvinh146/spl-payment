df41fafe8f24f0d4557ce151e5085a26deffe24c
tranvinh146 <vinh.tt@geekup.vn>
2022-12-05 22:45:54 +0700

    finalized

---
+++ lib/addresses.ts
@@ -0,0 +1,13 @@
+import { PublicKey } from '@solana/web3.js'
+
+export const shopAddress = new PublicKey(
+  '9oqYeLPM1apEg33XoNh86ZDBDhqrdtVa7tABmHAodpJX'
+)
+
+export const usdcAddress = new PublicKey(
+  'CXQYDT9ShDYG1JMMwjNiR6TcL4u4uJNnguAbLKw6jVv4'
+)
+
+export const couponAddress = new PublicKey(
+  '2SjswtGRxe89j7RT9EnB5zYGUMLepjRNieYQQoYvCc9F'
+)
